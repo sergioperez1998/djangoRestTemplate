51a58b4adad31525e75434f7a760cba6ec579a52
Sergio Pérez Martín <sergiopm150898@gmail.com>
2024-11-25 21:08:49 +0100

    bbdd conection

--- djangoTemplate/djangoTemplate/settings.py
+++ djangoTemplate/djangoTemplate/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangoTemplate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'sergio',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
 TIME_ZONE = 'UTC'
 
